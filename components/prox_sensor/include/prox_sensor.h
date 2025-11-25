ff7f254f3a0cd0428e35ee8d441093100db61468
agus-darmawan <wayanagus.dr@gmail.com>
2025-11-25 17:37:14 +0700

    feat: add prox sensor dyoygen

--- components/prox_sensor/include/prox_sensor.h
+++ components/prox_sensor/include/prox_sensor.h
@@ -1,3 +1,13 @@
+/**
+ * @file prox_sensor.h
+ * @author @agus-darmawan
+ * @brief This file contains the interface for the proximity opto sensor module
+ * @version 0.1
+ * @date 2025-11-25
+ * @copyright Copyright (c) 2025
+ */
+
+
 #pragma once
 #include <stdint.h>
 #include "driver/gpio.h"
@@ -7,6 +17,9 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Proximity sensor configuration structure
+ */
 typedef struct {
     gpio_num_t pin;
     gpio_int_type_t trigger;
@@ -14,14 +27,14 @@ typedef struct {
 } proximity_config_t;
 
 /**
- * Initialize proximity opto sensor (ISR).
+ * @brief Initialize proximity opto sensor (ISR).
  * @param cfg pointer to config
  * @return ESP_OK on success
  */
 esp_err_t proximity_init(const proximity_config_t *cfg);
 
 /**
- * Return total pulses since boot (one pulse = one wheel revolution).
+ * @brief Return total pulses since boot (one pulse = one wheel revolution).
  * @return pulse count
  */
 uint32_t proximity_get_count(void);
